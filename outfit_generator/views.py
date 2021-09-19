68d7ec77119484fd07d55b1cb7e6f92757ccfb30
Alex M <alex@matros.ca>
2021-09-19 02:22:34 -0400

    random outfit generator

--- outfit_generator/views.py
+++ outfit_generator/views.py
@@ -8,7 +8,7 @@ def select_style(request):
     return render(request, "outfit_generator/select_style.html")
 
 def outfit_generator(request, style):
-    style_name = style[style.index("-") + 1:style.rindex("-")]
+    style_name = style[style.index("-") + 1:style.rindex("-")].replace("-", " ")
     return render(request, "outfit_generator/outfit_generator.html", {
         "style": style_name
     })
